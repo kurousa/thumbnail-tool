609255226a93a5300aa143fd65180dd4485af7b3
kurousa <r.takahashi.1026@gmail.com>
2025-05-18 01:38:22 +0900

    feat: 様々な実装パターンを試す - channelを用いた、Producer-Consumerパターンでの実装 - rayonクレートを用いた実装

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use clap::Parser;
 use std::{
     fs::{create_dir_all, read_dir, remove_dir_all},
     path::PathBuf,
-    sync::{Arc, Mutex},
-    thread,
 };
 
 #[derive(Parser)]
@@ -30,44 +28,113 @@ fn main() {
     }
     create_dir_all(&args.output).unwrap();
 
-    let mut all_paths = vec![];
-    for entry in read_dir(&args.input).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_dir() {
-            continue;
+    // 1． スレッド処理を分割して、各スレッドが独立して処理を行う実装
+    // Arc, Mutexを使って、スレッド間で共有するデータを管理する
+    // use std::sync::{Arc, Mutex};
+    // use std::thread;
+    // let mut handles = vec![];
+    // let mut all_paths = vec![];
+    // for entry in read_dir(&args.input).unwrap() {
+    //     let entry = entry.unwrap();
+    //     let path = entry.path();
+    //     if path.is_dir() {
+    //         continue;
+    //     }
+    //     all_paths.push(path);
+    // }
+
+    // let processed_count = Arc::new(Mutex::new(0));
+    // let chunk_size = (all_paths.len() + 3) / 4;
+
+    // for chunk in all_paths.chunks(chunk_size) {
+    //     let chunk = chunk.to_vec();
+    //     let processed_count = processed_count.clone();
+    //     let output = args.output.clone();
+    //     handles.push(thread::spawn(move || {
+    //         for path in chunk {
+    //             let output_path = output.join(path.file_name().unwrap());
+    //             let img = image::open(&path);
+    //             if let Ok(img) = img {
+    //                 let thumbnail = img.thumbnail(64, 64);
+    //                 thumbnail.save(output_path).unwrap();
+    //                 let mut writer = processed_count.lock().unwrap();
+    //                 *writer += 1;
+    //             }
+    //         }
+    //     }))
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // println!(
+    //     "Processed {} images",
+    //     processed_count.as_ref().lock().unwrap()
+    // );
+
+    // 2. チャンネルを使って、スレッド間でメッセージを送受信する実装
+    // Producer-Consumerパターンを使用して、スレッド間でメッセージを送受信する
+    //
+    // use std::sync::mpsc::channel;
+    // let mut handles = vec![];
+    // let mut channels = vec![];
+    // let (counter_tx, counter_rx) = channel::<usize>();
+
+    // // recv側(サムネイル生成処理側)の立ち上げ
+    // for _ in 0..4 {
+    //     let (tx, rx) = channel::<PathBuf>();
+    //     channels.push(tx);
+    //     let counter_tx = counter_tx.clone();
+    //     let output = args.output.clone();
+    //     handles.push(thread::spawn(move || {
+    //         while let Ok(path) = rx.recv() {
+    //             let output_path = output.join(path.file_name().unwrap());
+    //             let img = image::open(&path);
+    //             if let Ok(img) = img {
+    //                 let thumbnail = img.thumbnail(64, 64);
+    //                 thumbnail.save(output_path).unwrap();
+    //                 counter_tx.send(1).unwrap();
+    //             }
+    //         }
+    //     }))
+    // }
+
+    // // send側(ファイル読み込み処理側)の立ち上げ
+    // // 処理対象ファイルパスの送信
+    // for (index, entry) in read_dir(&args.input).unwrap().enumerate() {
+    //     let entry = entry.unwrap();
+    //     let path = entry.path();
+    //     if path.is_dir() {
+    //         continue;
+    //     }
+    //     channels[index % channels.len()].send(path).unwrap();
+    // }
+    // // すべてのファイルを送信した後、各スレッドに終了を通知
+    // for channel in channels {
+    //     drop(channel);
+    // }
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+    // let processed_count = counter_rx.iter().count();
+    // println!("Processed {} images", processed_count);
+
+    // 3. rayonを使って、並列処理を行う実装
+    use rayon::prelude::*;
+    let items: Vec<_> = read_dir(&args.input).unwrap().collect();
+    let result = items.into_par_iter().map(|item| {
+        let item = item.unwrap();
+        let path = item.path();
+        let output_path = args.output.join(path.file_name().unwrap());
+        let img = image::open(&path);
+        if let Ok(img) = img {
+            let thumbnail = img.thumbnail(64, 64);
+            thumbnail.save(&output_path).unwrap();
+            1
+        } else {
+            0
         }
-        all_paths.push(path);
-    }
-
-    let processed_count = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
-    let chunk_size = (all_paths.len() + 3) / 4;
-
-    for chunk in all_paths.chunks(chunk_size) {
-        let chunk = chunk.to_vec();
-        let processed_count = processed_count.clone();
-        let output = args.output.clone();
-        handles.push(thread::spawn(move || {
-            for path in chunk {
-                let output_path = output.join(path.file_name().unwrap());
-                let img = image::open(&path);
-                if let Ok(img) = img {
-                    let thumbnail = img.thumbnail(64, 64);
-                    thumbnail.save(output_path).unwrap();
-                    let mut writer = processed_count.lock().unwrap();
-                    *writer += 1;
-                }
-            }
-        }))
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!(
-        "Processed {} images",
-        processed_count.as_ref().lock().unwrap()
-    );
+    });
+    println!("Processed {} images", result.sum::<u32>());
 }
